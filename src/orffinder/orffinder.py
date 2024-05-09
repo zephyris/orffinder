2e553f9ca2627b0b225edeb96e5d699ef9b45dba
Ulrich Dobramysl <1979498+ulido@users.noreply.github.com>
2024-05-09 09:10:39 +0000

    Fix error when running `python -m orffinder ...` by giving `orffinder` an entry point

--- src/orffinder/orffinder.py
+++ src/orffinder/orffinder.py
@@ -487,16 +487,3 @@ class Fasta:
     
     def fasta(self):
         return "\n".join([">" + x[0] + "\n" + x[1] for x in self.sequences.items()])
-
-if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        exit("Usage: python3 orffinder.py reference_genomes.fasta query_sequences.fasta [best|all]")
-    genome, query = sys.argv[1], sys.argv[2]
-    runtypes = ["best", "all"]
-    runtype = sys.argv[3]
-    if runtype not in runtypes:
-        exit("Error: Unrecognised run type: " + runtype)
-    if runtype == "best":
-        OrfFinder(genome, query).bestOrf()
-    elif runtype == "all":
-        OrfFinder(genome, query).allGoodOrfs()
