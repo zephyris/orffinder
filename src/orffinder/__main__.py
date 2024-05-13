39ce482ca5944ed4b7edd1eff19b8f50d187ed72
Ulrich Dobramysl <1979498+ulido@users.noreply.github.com>
2024-05-13 08:22:58 +0000

    Change "possible non-overlapping ORFs" to "good non-overlapping ORFs" in help message.
    Also refer to (still non-existant) documentation.

--- src/orffinder/__main__.py
+++ src/orffinder/__main__.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find open reading frames (ORFs) in the given sequence.")
     parser.add_argument('reference_genome', type=str, help="""The path to a FASTA file containing the reference genome used to cross-reference found ORFs against using tBLASTn.""")
     parser.add_argument('query_sequences', type=str, help="""The path to a FASTA file containing sequences ORFs should be found in. This could be e.g. a full genome or a set of mRNA sequences.""")
-    parser.add_argument('--mode', type=str, choices=['best', 'all'], default='all', help="The mode of operation: 'best' finds the best ORF in a given sequence, while 'all' finds all possible non-overlapping ORFs in a given sequence. Default is 'all'.")
+    parser.add_argument('--mode', type=str, choices=['best', 'all'], default='all', help="The mode of operation: 'best' finds the best ORF in a given sequence, while 'all' finds all good non-overlapping ORFs in a given sequence (see the documentation for details). Default is 'all'.")
 
     args = parser.parse_args()
     print()
