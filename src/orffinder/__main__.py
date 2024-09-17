902696b6bb9e521d2abe87ab6c09f9fc94dd5227
Ulrich Dobramysl <1979498+ulido@users.noreply.github.com>
2024-09-17 10:10:25 +0000

    GFF output command line parameter

--- src/orffinder/__main__.py
+++ src/orffinder/__main__.py
@@ -1,3 +1,5 @@
+from contextlib import nullcontext
+from orffinder.gffoutput import GffOutput
 from .orffinder import *
 import argparse
 
@@ -6,14 +8,23 @@ def main():
     parser.add_argument('reference_genome', type=str, help="""The path to a FASTA file containing the reference genome used to cross-reference found ORFs against using tBLASTn.""")
     parser.add_argument('query_sequences', type=str, help="""The path to a FASTA file containing sequences ORFs should be found in. This could be e.g. a full genome or a set of mRNA sequences.""")
     parser.add_argument('--mode', type=str, choices=['best', 'all'], default='all', help="The mode of operation: 'best' finds the best ORF in a given sequence, while 'all' finds all good non-overlapping ORFs in a given sequence (see the documentation for details). Default is 'all'.")
+    parser.add_argument('--output', type=str, default='-')
 
     args = parser.parse_args()
-    print()
     orffinder = OrfFinder(args.reference_genome, args.query_sequences)
     if args.mode == "best":
         results = orffinder.bestOrf()
     elif args.mode == "all":
         results = orffinder.allGoodOrfs()
+
+    gff = GffOutput(args.query_sequences)
+    for orfs in results.values():
+        for orf in orfs:
+            gff.add_orf(orf)
+    
+    gff_str = gff.save()
+    with open(args.output, 'w') if args.output != '-' else nullcontext(sys.stdout) as f:
+        f.write(gff_str)
 
 if __name__ == "__main__":
     main()
